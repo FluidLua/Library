33ecf5a3f7fb176aba457b447accaf3fe3b60136
R H <165597973+rhuda21@users.noreply.github.com>
2026-08-28 20:37:30 +0100

    Create Enchants.lua

---
+++ Game Data/Saber Simulator/PetInfo/Enchants.lua
@@ -0,0 +1,27 @@
+local v1 = {}
+v1["Coins I"] = {Rarity = 1, CoinsMulti = 0.05, Odds = 10.35, Desc = "Pet's coin multi increased by 5%"}
+v1["Coins II"] = {Rarity = 2, CoinsMulti = 0.1, Odds = 5.92, Desc = "Pet's coin multi increased by 10%"}
+v1["Coins III"] = {Rarity = 3, CoinsMulti = 0.15, Odds = 2.96, Desc = "Pet's coin multi increased by 15%"}
+v1["DNA I"] = {Rarity = 1, StorageMulti = 0.05, Odds = 10.35, Desc = "Pet gives 5% storage boost"}
+v1["DNA II"] = {Rarity = 2, StorageMulti = 0.1, Odds = 5.92, Desc = "Pet gives 10% storage boost"}
+v1["DNA III"] = {Rarity = 3, StorageMulti = 0.15, Odds = 2.96, Desc = "Pet gives 15% storage boost"}
+v1["Strength I"] = {Rarity = 1, StrengthMulti = 0.05, Odds = 10.35, Desc = "Pet's strength multi increased by 5%"}
+v1["Strength II"] = {Rarity = 2, StrengthMulti = 0.1, Odds = 5.92, Desc = "Pet's strength multi increased by 10%"}
+v1["Strength III"] = {Rarity = 3, StrengthMulti = 0.15, Odds = 2.96, Desc = "Pet's strength multi increased by 15%"}
+v1["Crowns I"] = {Rarity = 1, CrownsMulti = 0.05, Odds = 10.35, Desc = "Pet's crown multi increased by 5%"}
+v1["Crowns II"] = {Rarity = 2, CrownsMulti = 0.1, Odds = 5.92, Desc = "Pet's crown multi increased by 10%"}
+v1["Crowns III"] = {Rarity = 3, CrownsMulti = 0.15, Odds = 2.96, Desc = "Pet's crown multi increased by 15%"}
+v1["Genius I"] = {Rarity = 2, PetXPMulti = 0.05, Odds = 5.92, Desc = "Pet gains xp 5% faster"}
+v1["Genius II"] = {Rarity = 3, PetXPMulti = 0.1, Odds = 2.96, Desc = "Pet gains xp 10% faster"}
+v1["Genius III"] = {Rarity = 4, PetXPMulti = 0.15, Odds = 0.89, Desc = "Pet gains xp 15% faster"}
+v1["Damage I"] = {Rarity = 2, ExtraDamage = 2, Odds = 4.73, Desc = "Pet gives +2 damage to bosses & mobs"}
+v1["Damage II"] = {Rarity = 3, ExtraDamage = 3, Odds = 2.96, Desc = "Pet gives +3 damage to bosses & mobs"}
+v1["Damage III"] = {Rarity = 4, ExtraDamage = 5, Odds = 0.89, Desc = "Pet gives +5 damage to bosses & mobs"}
+v1["Lucky I"] = {Rarity = 3, LuckMulti = 0.05, Odds = 2.96, Desc = "Pet gives 0.05x luck"}
+v1["Lucky II"] = {Rarity = 4, LuckMulti = 0.1, Odds = 0.89, Desc = "Pet gives 0.10x luck"}
+v1["Lucky III"] = {Rarity = 5, LuckMulti = 0.15, Odds = 0.44, Desc = "Pet gives 0.15x luck"}
+v1.Elemental = {Rarity = 6, ElementPointMulti = 0.15, Odds = 0.15, Desc = "Pet gives 15% more element points"}
+v1.Reaper = {Rarity = 6, BossDropsMulti = 0.2, Odds = 0.16, Desc = "Pet gives 20% more drops from normal bosses"}
+v1["Secret Hunter"] = {Rarity = 7, SecretMulti = 0.25, Odds = 0.09, Desc = "Pet gives 25% more chance to hatch secrets"}
+v1["Limit Breaker"] = {Rarity = 7, PetMaxLevelIncrease = 25, Odds = 0.07, Desc = "Pet's max level increased by 25"}
+return v1
